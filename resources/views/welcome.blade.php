6e600b7c588cd3caa26b8f524a56e02c2f724420
kludsh <yozanirwan@gmail.com>
2021-11-17 15:28:50 +0700

    Push it up

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+
+    <h1>Setup Laravel Bootstrap Successfully <i class="fas fa-ad"></i></h1>
+       
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div> 
+    </div>
+        <script src="{{ Mix('js/app.js') }}"></script>
+
+    </body>
+</html>
