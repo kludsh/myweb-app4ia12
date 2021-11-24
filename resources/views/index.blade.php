fd604301588634afd7343efbaed33a92a5d2070c
kludsh <yozanirwan@gmail.com>
2021-11-24 14:36:10 +0700

    Menambahkan Folder dan file pada includes dan pages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -24,4 +24,21 @@
         <script src="{{ Mix('js/app.js') }}"></script>
 
     </body>
+</html> -->
+<!doctype html>
+<html>
+  <head>
+    @include('includes.head')
+  </head>
+  <body>
+      @include('includes.navbar')
+      <div class="wrap container">
+        <div id="main" class="row">
+          <div class="col-md-12">
+            @include('pages.header')
+          </div>
+        </div>
+      </div>
+      @include('includes.footer')
+  </body>
 </html>
